ef7e11eeee033920008c4692a39ce112a1319e94
belloluca <belloluca2002@gmail.com>
2026-06-03 19:50:42 +0200

    Update app.py
    Test delle modalità

--- ai-coding-tutor/backend/app.py
+++ ai-coding-tutor/backend/app.py
@@ -1,21 +1,72 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/chat", methods=["POST"])
+@app.route('/api/chat', methods=['POST'])
 def chat():
     data = request.get_json()
+    message = data.get('message', '')
+    mode = data.get('mode', 'Tutor')
 
-    message = data.get("message", "")
-    mode = data.get("mode", "tutor")
+    if mode == 'Tutor':
+        response = f""" 
+        Modalità Tutor.
 
-    response = f"Studente: {message}. Modalità selezionata: {mode}"
+        Ti spiego il seguente argomento:
+            {message}
+
+        Spiegazione
+
+        """
+    elif mode == 'Quiz':
+        response = f""" 
+
+        Modalità Quiz.
+
+        In base alla richiesta:
+            {message}
+
+        Esercizio:
+            A)
+            B)
+            C)
+            D)
+
+        """
+    elif mode == 'Debug codice':
+        response = f"""
+
+        Modalita di Debug del codice.
+
+        Codice inserito o richiesta:
+            {message}
+
+        Suggerimenti:
+            ....
+            ....
+    
+        """
+
+    elif mode == 'Esercizi':
+        response = f""" 
+
+        Modalità Esercizi.
+
+        In base alla richiesta:
+            {message}
+
+        Esercizio:
+            Svolgi la somma
+
+        """
+    else:
+            response = "Modalità non riconosciuta"
 
     return jsonify({
-        "response" : response
+        "response": f"Hai scritto: {message}. Modalità: {mode}"
     })
 
-    if __name__ == "__main__":
-        app.run(debug = True, port = 5000)
+if __name__ == '__main__':
+    app.run(debug=True)
